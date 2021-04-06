108fe6eed04f7d388ff5c7adf50e53d21bbde6f5
Leonrique Oliveira <leonrick777@gmail.com>
2021-04-05 21:53:37 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,61 @@
-import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { Component, ElementRef, ViewChild  } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import {Item } from '../app/models/Item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   items: any[];
+  suggestions: any[];
+  db: AngularFirestore;
+  itemsCollection: AngularFirestoreCollection<Item>;
+  
+  item: Item ={
+    who: '', item: ''
+  }
+  @ViewChild('main-ul') nameUL: ElementRef;
+  
+  constructor(db: AngularFirestore){
+    
+    this.itemsCollection = db.collection('items');
 
-  constructor(db: AngularFireDatabase){
-    db.list('items').valueChanges()
+    db.collection('items').snapshotChanges()
       .subscribe(items => {
-        this.items = items;
+        this.items = items.map(v => {
+          const data = v.payload.doc.data();
+          return data;
+        });
         console.log(this.items);
       });
+
+      db.collection('suggestions').valueChanges()
+      .subscribe(suggestions => {
+        this.suggestions = suggestions;
+        console.log(this.suggestions);
+      });
+  }
+
+  saveData(){
+    this.itemsCollection  .add({item: 'novo', who: 'leon2'});
+    console.log(this.itemsCollection);
+    console.log(this.item);
+  }
+
+  SetBorder(value){
+    // console.log(value);
+    value.srcElement.classList.toggle('selected')
+    // document.querySelector("#main-ul").classList.add('selected');
+  }
+
+  ngOnInit(): void {
+    const cbox = document.querySelectorAll("li");
+
+    for (let i = 0; i < cbox.length; i++) {
+        cbox[i].addEventListener("click", this.SetBorder);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire'
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-//import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
